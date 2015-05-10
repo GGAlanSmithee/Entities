4d786d68a3ee902a706519863ccb5902c66ea9b8
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-05-10 09:02:46 +0000

    moves back to passing in world as an constructor argument to entity factory
    since there were problems with the fluent API when delegating calls from
    entity manager to entity factory, entity factory needs to be tightly coupled
    to a world
    
    the management of changing a world should be done by/through the entity manager

--- src/core/EntityFactory.js
+++ src/core/EntityFactory.js
@@ -4,13 +4,30 @@
 * @license      {@link https://github.com/GGAlanSmithee/Entities/blob/master/LICENSE|MIT License}
 */
 
-Entities.EntityFactory = function() {
+Entities.EntityFactory = function(world) {
+    if (world === undefined || world === null || !(world instanceof Entities.World)) {
+        throw 'An entity factory requires a world to work with';
+    }
+        
+    this.World         = world;
     this.Initializers  = {};
     this.Configuration = {};
 };
 
 Entities.EntityFactory.prototype = {
     constructor : Entities.EntityFactory,
+    
+    registerInitializer : function(componentType, initializer) {
+        if (!componentType) {
+            return;
+        }
+        
+        if (!initializer || !(typeof(initializer) === 'function')) {
+            return;
+        }
+        
+        this.Initializers[componentType.name] = initializer;
+    },
     
     build : function() {
         this.Configuration = {};
@@ -19,7 +36,6 @@ Entities.EntityFactory.prototype = {
     },
     
     withComponent : function(componentType, initializer) {
-        // todo make a general reset function if no initializer function is passed in
         this.Configuration[componentType.name] = {
             type : componentType,
             initializer : initializer && typeof(initializer) === 'function' ?
@@ -27,7 +43,7 @@ Entities.EntityFactory.prototype = {
                               this.Initializers[componentType.name] ?
                                   this.Initializers[componentType.name] :
                                   function(component) {
-                                      // reset here
+                                      // todo make a generic reset function if no initializer function is passed in
                                   }
         };
         
@@ -38,20 +54,16 @@ Entities.EntityFactory.prototype = {
         return this.Configuration;
     },
     
-    create : function(world, count, configuration) {
-        if (world === undefined || world === null || !(world instanceof Entities.World)) {
-            throw 'An entity factory requires a world to work with';
-        }
-        
+    create : function(count, configuration) {
         let createdEntities = [];
         
         count = count ? count : 1;
         
         for (let i = 0; i < count; ++i)
         {
-            let entity = world.getFirstUnusedEntity();
+            let entity = this.World.getFirstUnusedEntity();
         
-            if (entity >= world.Capacity) {
+            if (entity >= this.World.Capacity) {
                 break;
             }
             
@@ -64,36 +76,32 @@ Entities.EntityFactory.prototype = {
             Object.keys(configuration).forEach(function(key) {
                 let conf = configuration[key];
                 
-                if (world.Type !== Entities.World.Type.Static) {
-                    world.createComponent(conf.type, entity);
+                if (this.World.Type !== Entities.World.Type.Static) {
+                    this.World.createComponent(conf.type, entity);
                 }
                 
-                entityComponentIdentifier = entityComponentIdentifier | world.ComponentType[key];
+                entityComponentIdentifier = entityComponentIdentifier | this.World.ComponentType[key];
                 
-                conf.initializer(world[key][entity]);
+                conf.initializer(this.World[key][entity]);
             }, this);
             
-            world.useEntity(entity, entityComponentIdentifier);
+            this.World.useEntity(entity, entityComponentIdentifier);
             
             createdEntities.push(entity);
         }
         
         return createdEntities;
-    },
-    
-    registerInitializer : function(componentType, initializer) {
-        if (!componentType) {
-            return;
-        }
-        
-        if (!initializer || !(typeof(initializer) === 'function')) {
-            return;
-        }
-        
-        this.Initializers[componentType.name] = initializer;
     }
 };
 
+Object.defineProperty(Entities.EntityFactory.prototype, 'World', {
+    get: function() {
+        return this._world;
+    },
+    set: function(world) {
+        this._world = world;
+    }
+});
 Object.defineProperty(Entities.EntityFactory.prototype, 'Configuration', {
     get: function() {
         return this._configuration;
