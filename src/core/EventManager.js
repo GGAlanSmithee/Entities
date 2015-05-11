61f2c0397195c03b915b3d2798a67f83b7e20d75
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-05-11 06:28:02 +0000

    adds event manager class stub

---
+++ src/core/EventManager.js
@@ -0,0 +1,13 @@
+/**
+* @author       Alan Smithee <ggnore.alan.smithee@gmail.com>
+* @copyright    2015 GGNoRe.
+* @license      {@link https://github.com/GGAlanSmithee/Entities/blob/master/LICENSE|MIT License}
+*/
+
+Entities.EventManager = function() {
+    return this;
+};
+
+Entities.EventManager.prototype = {
+    constructor : Entities.EventManager
+};
