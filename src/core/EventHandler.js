2a20cdac10f912853629fd340fd866ba6894f4f7
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-05-16 10:03:46 +0000

    adds delayed triggering and callback functionality to event handler
    the callback functionality is implemented through promises which gives
    a nice interface for the user to work with.
    
    the callback (chained with a 'then' method) is invoked after all listeners
    have been executed.
    
    also fixes an issue were a listener was never really removed when stopListening
    was called.

--- src/core/EventHandler.js
+++ src/core/EventHandler.js
@@ -9,6 +9,12 @@ Entities.EventHandler = function() {
     return this;
 };
 
+Entities.EventHandler.EmptyPromise = function() {
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
+};
+
 Entities.EventHandler.prototype = {
     constructor : Entities.EventHandler,
     
@@ -29,35 +35,84 @@ Entities.EventHandler.prototype = {
             return;
         }
         
-        if (!this.events[event]) {
+        if (!this.events[event] || !this.events[event][handler]) {
             return;
         }
         
-        this.events[event][handler] = null;
+        delete this.events[event][handler];
     },
     
     trigger : function() {
-        let event = Array.prototype.splice.call(arguments, 0, 1)[0];
+        let args  = arguments;
+        let event = Array.prototype.splice.call(args, 0, 1)[0];
         
         if (typeof event !== 'string') {
-            return;
+            return Entities.EventHandler.EmptyPromise();
         }
         
-        let self = this instanceof Entities.EntityManager ? this.eventHandler : this;
+        let self    = this instanceof Entities.EntityManager ? this.eventHandler : this;
+        let context = this;
         
         if (!self.events[event]) {
-            return;
+            return Entities.EventHandler.EmptyPromise();
         }
         
-        let keys = Object.keys(self.events[event]);
+        let keys     = Object.keys(self.events[event]);
+        let promises = [];
         
         let i = keys.length - 1;
         while (i >= 0) {
             if (self.events[event][keys[i]]) {
-                self.events[event][keys[i]].apply(this, arguments);
+                promises.push(new Promise(function(resolve, reject) {
+                    resolve(self.events[event][keys[i]].apply(context, args));
+                }));
             }
             
             --i;
         }
-    }
+        
+        return Promise.all(promises);
+    },
+    
+    triggerDelayed : function() {
+        let args  = arguments;
+        let event = Array.prototype.splice.call(args, 0, 1)[0];
+        
+        if (typeof event !== 'string') {
+            return Entities.EventHandler.EmptyPromise();
+        }
+        
+        let timeout = Array.prototype.splice.call(args, 0, 1)[0];
+        
+        if (typeof timeout !== 'number') {
+            return Entities.EventHandler.EmptyPromise();
+        }
+        
+        let self    = this instanceof Entities.EntityManager ? this.eventHandler : this;
+        let context = this;
+        
+        if (!self.events[event]) {
+            return Entities.EventHandler.EmptyPromise();
+        }
+        
+        let keys     = Object.keys(self.events[event]);
+        let promises = [];
+        
+        let i = keys.length - 1;
+        while (i >= 0) {
+            if (self.events[event][keys[i]]) {
+                (function(index) {
+                    promises.push(new Promise(function(resolve, reject) {
+                        setTimeout(function(){
+                            resolve(self.events[event][keys[index]].apply(context, args));
+                        }, timeout);
+                    }));
+                })(i);
+            }
+            
+            --i;
+        }
+        
+        return Promise.all(promises);
+    },
 };
