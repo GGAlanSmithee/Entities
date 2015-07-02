5614a0961f38a98d708574abcece7fb895450acc
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-07-02 21:01:51 +0000

    adds more tests for EntityFactory.create() #12 #14

--- test/entity/factory/Create.js
+++ test/entity/factory/Create.js
@@ -69,5 +69,106 @@ describe('EntityFactory', function() {
             expect(entity).to.have.property(this.componentFour);
             expect(entity).property(this.componentFour).to.equal(this.componentFourInitializer());
         });
+        
+        it('does not change a components value if there is no initializer for that component', () => {
+            this.entityFactory.configuration.set(this.componentOne, undefined);
+            this.entityFactory.configuration.set(this.componentTwo, undefined);
+            this.entityFactory.configuration.set(this.componentThree, undefined);
+            this.entityFactory.configuration.set(this.componentFour, undefined);
+            
+            let entity = this.entityFactory.create(this.world)[0];
+
+            expect(this.newEntityStub.calledOnce).to.be.true;
+            
+            expect(entity).to.have.property('id');
+            
+            expect(entity).to.have.property(this.componentOne);
+            expect(entity).property(this.componentOne).property('x').to.equal(1.0);
+            
+            expect(entity).to.have.property(this.componentTwo);
+            expect(entity).property(this.componentTwo).property('y').to.equal(2.0);
+            
+            expect(entity).to.have.property(this.componentThree);
+            expect(entity).property(this.componentThree).to.equal(1);
+            
+            expect(entity).to.have.property(this.componentFour);
+            expect(entity).property(this.componentFour).to.equal("Name");
+        });
+        
+        it('creates an entity from a [configuration]', () => {
+            let configuration = this.entityFactory.configuration;
+            
+            this.entityFactory.configuration = new Map();
+            
+            let entities = this.entityFactory.create(this.world);
+            
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create(this.world, 1, configuration);
+
+            expect(entities).property('length').to.equal(1);
+
+            let entity = entities[0];
+            
+            expect(this.newEntityStub.calledTwice).to.be.true;
+            
+            expect(entity).to.have.property('id');
+            
+            expect(entity).to.have.property(this.componentOne);
+            expect(entity).property(this.componentOne).property('x').to.equal(4.0);
+            
+            expect(entity).to.have.property(this.componentTwo);
+            expect(entity).property(this.componentTwo).property('y').to.equal(5.0);
+            
+            expect(entity).to.have.property(this.componentThree);
+            expect(entity).property(this.componentThree).to.equal(this.componentThreeInitializer());
+            
+            expect(entity).to.have.property(this.componentFour);
+            expect(entity).property(this.componentFour).to.equal(this.componentFourInitializer());
+        });
+        
+        it('creates [count] number of entities', () => {
+            let count = Math.floor(Math.random() * 100);
+            expect(this.entityFactory.create(this.world, count)).property('length').to.equal(count);
+        });
+        
+        it('creates [count] number of entities from [configuration]', () => {
+            let configuration = this.entityFactory.configuration;
+            
+            this.entityFactory.configuration = new Map();
+            
+            let count = Math.floor(Math.random() * 100);
+            expect(this.entityFactory.create(this.world, count, configuration)).property('length').to.equal(count);
+        });
+        
+        it('returns an empty array if [world] is not an instance of World', () => {
+            let entities = this.entityFactory.create();
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create(new Map());
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create([]);
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create({});
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create(null);
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create(1);
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+            
+            entities = this.entityFactory.create('not a World instance.');
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(0);
+        });
     });
 });
