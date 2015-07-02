d24e4dbda1fce811fe9db85139a0b6420f1dc5a0
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-07-02 21:14:21 +0000

    adds test for EntityFactory method chaining

---
+++ test/entity/factory/MethodChaining.js
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import { EntityFactory } from '../../../src/core/Entity';
+import World from '../../../src/core/World';
+
+describe('EntityFactory', function() {
+    describe('method chaining', () => {
+        beforeEach(() => {
+            this.entityFactory = new EntityFactory();
+            this.world = new World();
+            
+            this.componentOne = this.world.registerComponentType({ x : 10 });
+            this.componentTwo = this.world.registerComponentType('From component');
+            
+            this.entityFactory.registerInitializer(this.componentOne, function() { this.x = 20; });
+            this.entityFactory.registerInitializer(this.componentTwo, function() { return 'From initializer'; });
+        });
+        
+        afterEach(() => {
+            delete this.entityFactory;
+            delete this.world;
+        });
+        
+        it('allows for an entity to be created by chained method calls', () => {
+            let entities = this.entityFactory.build()
+                                             .withComponent(this.componentOne)
+                                             .withComponent(this.componentTwo)
+                                             .create(this.world);
+                                           
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(1);
+            
+            let entity = entities[0];
+            
+            expect(entity).to.be.an.instanceof(Object);
+            
+            expect(entity).to.have.property(this.componentOne);
+            expect(entity).property(this.componentOne).property('x').to.equal(20);
+            
+            expect(entity).to.have.property(this.componentTwo);
+            expect(entity).property(this.componentTwo).to.equal('From initializer');
+        });
+        
+    });
+});
