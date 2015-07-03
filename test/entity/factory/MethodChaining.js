3f5585bcd23cd8c893fbb8d1456ccc60363c6bb7
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-07-03 08:06:20 +0000

    adds more tests for EntityFactory #12 #14

--- test/entity/factory/MethodChaining.js
+++ test/entity/factory/MethodChaining.js
@@ -40,5 +40,48 @@ describe('EntityFactory', function() {
             expect(entity).property(this.componentTwo).to.equal('From initializer');
         });
         
+        it('allows for overridden configurations when creating an entity by chained method calls', () => {
+            let entities = this.entityFactory.build()
+                                             .withComponent(this.componentOne, function() { this.x = 200; })
+                                             .withComponent(this.componentTwo, () => { return 'From overridden initializer'; })
+                                             .create(this.world);
+                                           
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(1);
+            
+            let entity = entities[0];
+            
+            expect(entity).to.be.an.instanceof(Object);
+            
+            expect(entity).to.have.property(this.componentOne);
+            expect(entity).property(this.componentOne).property('x').to.equal(200);
+            
+            expect(entity).to.have.property(this.componentTwo);
+            expect(entity).property(this.componentTwo).to.equal('From overridden initializer');
+        });
+        
+        it('allows for an entity to be created from a passed in configuration', () => {
+            let configuration = this.entityFactory.build()
+                                             .withComponent(this.componentOne)
+                                             .withComponent(this.componentTwo)
+                                             .createConfiguration();
+                                           
+            this.entityFactory.configuration = new Map();
+            
+            let entities = this.entityFactory.create(this.world, 1, configuration);
+            
+            expect(entities).to.be.an.instanceof(Array);
+            expect(entities).property('length').to.equal(1);
+            
+            let entity = entities[0];
+            
+            expect(entity).to.be.an.instanceof(Object);
+            
+            expect(entity).to.have.property(this.componentOne);
+            expect(entity).property(this.componentOne).property('x').to.equal(20);
+            
+            expect(entity).to.have.property(this.componentTwo);
+            expect(entity).property(this.componentTwo).to.equal('From initializer');
+        });
     });
 });
