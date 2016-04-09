f7faa5bbd1506b726010f94464a4e50f8e7a9b40
Alan Smithee <ggnore.alan.smithee@gmail.com>
2016-04-09 19:55:52 +0000

    adds some additional unit tests for newly added initSystems

--- test/system/register-system.js
+++ test/system/register-system.js
@@ -40,25 +40,37 @@ describe('SystemManager', function() {
             
             expect(system).to.equal(2);
             
-            system = this.systemManager.registerSystem(SystemType.Render, SelectorType.GetWith, 1 | 2, () => { });
+            system = this.systemManager.registerSystem(SystemType.Init, SelectorType.GetWith, 1 | 2, () => { });
             
             expect(system).to.equal(3);
             
-            system = this.systemManager.registerSystem(SystemType.Logic, SelectorType.GetWith, 1 | 2, () => { });
+            system = this.systemManager.registerSystem(SystemType.Render, SelectorType.GetWith, 1 | 2, () => { });
             
             expect(system).to.equal(4);
+            
+            system = this.systemManager.registerSystem(SystemType.Logic, SelectorType.GetWith, 1 | 2, () => { });
+            
+            expect(system).to.equal(5);
+            
+            system = this.systemManager.registerSystem(SystemType.Init, SelectorType.GetWith, 1 | 2, () => { });
+            
+            expect(system).to.equal(6);
         });
         
         it('adds a system with the correct [type] and [selector]', () => {
             let logicSystem   = this.systemManager.registerSystem(SystemType.Logic, SelectorType.Get, 1 | 2, () => { });
             let renderSystem  = this.systemManager.registerSystem(SystemType.Render, SelectorType.GetWith, 1 | 2, () => { });
+            let initSystem  = this.systemManager.registerSystem(SystemType.Init, SelectorType.GetWith, 1 | 2, () => { });
             let logicSystem2  = this.systemManager.registerSystem(SystemType.Logic, SelectorType.GetWithOnly, 1 | 2, () => { });
             let renderSystem2 = this.systemManager.registerSystem(SystemType.Render, SelectorType.GetWithout, 1 | 2, () => { });
+            let initSystem2  = this.systemManager.registerSystem(SystemType.Init, SelectorType.Get, 1 | 2, () => { });
 
             expect(this.systemManager.logicSystems.get(logicSystem)).property('selector').to.equal(SelectorType.Get);
             expect(this.systemManager.renderSystems.get(renderSystem)).property('selector').to.equal(SelectorType.GetWith);
+            expect(this.systemManager.initSystems.get(initSystem)).property('selector').to.equal(SelectorType.GetWith);
             expect(this.systemManager.logicSystems.get(logicSystem2)).property('selector').to.equal(SelectorType.GetWithOnly);
             expect(this.systemManager.renderSystems.get(renderSystem2)).property('selector').to.equal(SelectorType.GetWithout);
+            expect(this.systemManager.initSystems.get(initSystem2)).property('selector').to.equal(SelectorType.Get);
         });
         
         it('registeres a system with the correct correct [components] and [callback]', () => {
