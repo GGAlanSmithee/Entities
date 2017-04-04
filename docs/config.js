21aecc0836ee6041b5a7f03af8577027e7115d1d
Alan Smithee <ggnore.alan.smithee@gmail.com>
2017-04-04 14:27:47 +0000

    final correction of doc paths

--- docs/config.js
+++ docs/config.js
@@ -1,11 +1,12 @@
 window.self.$config = {
     home: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/README.md',
     nav: [
+        {title: 'Home', path: '/', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/README.md'},
         {title: 'API', path: '/api/api', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/api.md'},
         {title: 'EntityManager', path: '/api/entity-manager', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/entity-manager.md'},
         {title: 'EntityFactory', path: '/api/entity-factory', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/entity-factory.md'},
         {title: 'ComponentManager', path: '/api/component-manager', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/component-manager.md'},
-        {title: 'SystemManager', path: '/api/system-manager', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/system-maanger.md'},
+        {title: 'SystemManager', path: '/api/system-manager', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/system-manager.md'},
         {title: 'EventHandler', path: '/api/event-handler', source: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/docs/api/event-handler.md'}
     ]
 }
