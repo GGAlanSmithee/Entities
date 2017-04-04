6642db7fc6df736540c7dbf37320331354351bf0
Alan Smithee <ggnore.alan.smithee@gmail.com>
2017-04-04 14:05:22 +0000

    testign with absolute urls

--- docs/config.js
+++ docs/config.js
@@ -1,11 +1,11 @@
 window.self.$config = {
     home: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/README.md',
     nav: [
-        {title: 'API', path: 'api/api.md'},
-        {title: 'EntityManager', path: 'api/entity-manager.md'},
-        {title: 'EntityFactory', path: 'api/entity-factory.md'},
-        {title: 'ComponentManager', path: 'api/component-manager.md'},
-        {title: 'SystemManager', path: 'api/system-manager.md'},
-        {title: 'EventHandler', path: 'api/event-handler.md'},
+        {title: 'API', path: '/docs/api/api.md'},
+        {title: 'EntityManager', path: '/docs/api/entity-manager.md'},
+        {title: 'EntityFactory', path: '/docs/api/entity-factory.md'},
+        {title: 'ComponentManager', path: '/docs/api/component-manager.md'},
+        {title: 'SystemManager', path: '/docs/api/system-manager.md'},
+        {title: 'EventHandler', path: '/docs/api/event-handler.md'},
     ]
 }
