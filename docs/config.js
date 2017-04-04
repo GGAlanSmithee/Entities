1e85fe56c0d632588f3991c704da189c0232fbf6
Alan Smithee <ggnore.alan.smithee@gmail.com>
2017-04-04 14:03:19 +0000

    attempt to fix docs navigation

--- docs/config.js
+++ docs/config.js
@@ -1,11 +1,11 @@
 window.self.$config = {
     home: 'https://raw.githubusercontent.com/ggalansmithee/entities/master/README.md',
     nav: [
-        {title: 'API', path: '/api/api.md'},
-        {title: 'EntityManager', path: '/api/entity-manager.md'},
-        {title: 'EntityFactory', path: '/api/entity-factory.md'},
-        {title: 'ComponentManager', path: '/api/component-manager.md'},
-        {title: 'SystemManager', path: '/api/system-manager.md'},
-        {title: 'EventHandler', path: '/api/event-handler.md'},
+        {title: 'API', path: 'api/api.md'},
+        {title: 'EntityManager', path: 'api/entity-manager.md'},
+        {title: 'EntityFactory', path: 'api/entity-factory.md'},
+        {title: 'ComponentManager', path: 'api/component-manager.md'},
+        {title: 'SystemManager', path: 'api/system-manager.md'},
+        {title: 'EventHandler', path: 'api/event-handler.md'},
     ]
 }
